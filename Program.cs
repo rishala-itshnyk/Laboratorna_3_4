36a873c60fe653c11f9f01f2458de5460cef9119
Oleg Lysiak <lysiak.ob@gmail.com>
2023-10-15 12:18:33 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x; // вхідний аргумент
+        double y; // вхідний параметр
+        double R; // вхідний параметр
+        
+        
+        Console.Write("x = ");
+        x = double.Parse(Console.ReadLine());
+        Console.Write("y = ");
+        y = double.Parse(Console.ReadLine());
+        Console.Write("R = ");
+        R = double.Parse(Console.ReadLine());
+
+        // розгалуження в повній формі
+        if (((x + R) * (x + R) + (y - R) * (y - R) <= R * R) ||
+            ((y >= -R) && (y <= 0) && (x >= 0) && (x <= 2 * R)))
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+
+        Console.ReadLine();
+    }
+}
